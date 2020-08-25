42a2b74d0ef78cd2e2d4e45e0ecba8dc35251887
Sidak <sidak.pasrichassp@gmail.com>
2020-08-25 13:00:18 +0530

    Unit test CartController

---
+++ src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -0,0 +1,160 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.mockito.Mockito.*;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class CartControllerTest {
+
+    private CartController cartController;
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final CartRepository cartRepository = mock(CartRepository.class);
+    private final ItemRepository itemRepository = mock(ItemRepository.class);
+
+    @Before
+    public void setup(){
+        cartController = new CartController();
+        TestUtils.injectObjects(cartController, "userRepository", userRepository);
+        TestUtils.injectObjects(cartController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(cartController, "itemRepository",itemRepository);
+    }
+
+    @Test
+    public void addToCartNoUserError(){
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.addTocart(modifyCartRequest);
+
+        assertNotNull(responseEntity);
+        assertEquals(404, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void addToCartNoItemError(){
+        when(userRepository.findByUsername("Username")).thenReturn(new User());
+        when(itemRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("Username", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.addTocart(modifyCartRequest);
+
+        assertNotNull(responseEntity);
+        verify(itemRepository, times(1)).findById(1L);
+        assertEquals(404, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void addToCartHappyPath(){
+        User user = fakeUser();
+        Item item = fakeItem();
+        Cart cart = user.getCart();
+        when(userRepository.findByUsername("Username")).thenReturn(user);
+        when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
+
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("Username", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.addTocart(modifyCartRequest);
+
+        cart.addItem(item);
+
+        assertNotNull(responseEntity);
+        verify(cartRepository, times(1)).save(cart);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void removeFromCartNoUserError(){
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.removeFromcart(modifyCartRequest);
+
+        assertNotNull(responseEntity);
+        assertEquals(404, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void removeFromCartNoItemError(){
+        when(userRepository.findByUsername("Username")).thenReturn(new User());
+        when(itemRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("Username", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.removeFromcart(modifyCartRequest);
+
+        assertNotNull(responseEntity);
+        verify(itemRepository, times(1)).findById(1L);
+        assertEquals(404, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void removeFromCartHappyPath(){
+        User user = fakeUser();
+        Item item = fakeItem();
+        Cart cart = user.getCart();
+        cart.addItem(item);
+        user.setCart(cart);
+        when(userRepository.findByUsername("Username")).thenReturn(user);
+        when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
+
+        ModifyCartRequest modifyCartRequest = createModifyCartRequest("Username", 1, 1);
+        ResponseEntity<Cart> responseEntity = cartController.removeFromcart(modifyCartRequest);
+
+        cart.removeItem(item);
+
+        assertNotNull(responseEntity);
+        verify(cartRepository, times(1)).save(cart);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+    }
+
+    private Cart emptyCart(){
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setUser(null);
+        cart.setItems(new ArrayList<Item>());
+        cart.setTotal(BigDecimal.valueOf(0.0));
+        return cart;
+    }
+
+    private User fakeUser(){
+        User user = new User();
+        user.setId(1);
+        user.setUsername("Username");
+        user.setSalt("");
+        user.setPassword("Password");
+        user.setCart(emptyCart());
+
+        return user;
+    }
+
+    private Item fakeItem(){
+        Item item = new Item();
+        item.setId(1L);
+        item.setName("Fake Item");
+        item.setDescription("This is a fake");
+        item.setPrice(BigDecimal.valueOf(10.0));
+
+        return item;
+    }
+
+    private ModifyCartRequest createModifyCartRequest(String username, long itemId, int quantity){
+        ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+        modifyCartRequest.setUsername(username);
+        modifyCartRequest.setItemId(itemId);
+        modifyCartRequest.setQuantity(quantity);
+        return modifyCartRequest;
+    }
+}
